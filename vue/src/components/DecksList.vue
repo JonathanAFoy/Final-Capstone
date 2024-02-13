c23efab45a9114b0890d6e0140a1fcad406b7424
Jonathan Foy <foy.jon@gmail.com>
2024-02-13 14:26:10 -0500

    style updates

--- vue/src/components/DecksList.vue
+++ vue/src/components/DecksList.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="decks">
-      <router-link v-for="deck in decks" v-bind:key="deck.id"
-        v-bind:to="{ name: 'DeckView', params: { id: deck.id } }">
-        <div class="deck">{{ deck.title }}</div>
+      <router-link v-for="deck in decks" v-bind:key="deck.deckId"
+        v-bind:to="{ name: 'DeckView', params: { id: deck.deckId } }">
+        <div class="deck" v-bind:style="{}">{{ deck.title }}</div>
       </router-link>
     </div>
   </template>
@@ -14,6 +14,22 @@
   </script>
   
   <style scoped>
+
+  .decks { display: flex;
+  flex-wrap: wrap;
+  column-gap: 25px;
+  row-gap: 25px;
+  justify-content: flex-start;
+  align-items: center;
+}
+
+.deck {
+  min-width: 150px;
+  border-radius: 10px;
+  padding: 40px;
+  text-align: center;
+}
+
 
   </style>
   
--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -2,39 +2,43 @@
   <div class="home">
     <h1>My Cards and Decks</h1>
     <p>If you are seeing this, you are authenticated.</p>
-    <br>
-    <router-link v-bind:to="{name: 'create-deck'}">Create New Deck</router-link>
+    <br />
+    <router-link v-bind:to="{ name: 'create-deck' }"
+      >Create New Deck</router-link
+    >
+    <br />
+    <br />
+    <br />
+    <br />
 
-    <div class="deck" v-for="deck in deckList" v-bind:key="deck.id">
-    
-    {{ deck.deckId }} <br />
-    {{ deck.deckTitle }} <br />
-    {{ deck.username }} <br />
-    
+    <div class="deck-display">
+      <div class="deck" v-for="deck in deckList" v-bind:key="deck.id">
+        {{ deck.deckId }} <br />
+        {{ deck.deckTitle }} <br />
+        {{ deck.username }} <br />
+      </div>
     </div>
     <br />
     <br />
     <br />
 
-    <router-link v-bind:to="{name: 'create-card'}">Create New Card</router-link>
+    <router-link v-bind:to="{ name: 'create-card' }"
+      >Create New Card</router-link
+    >
     <div class="card" v-for="card in cardList" v-bind:key="card.id">
-
       {{ card.id }} <br />
       {{ card.title }} <br />
       {{ card.author }} <br />
-    
     </div>
-
   </div>
 </template>
 
 <script>
-import DecksList from '../components/DecksList.vue';
-import DeckService from "../services/DeckService.js"
-import CardService from "../services/CardService.js"
+import DecksList from "../components/DecksList.vue";
+import DeckService from "../services/DeckService.js";
+import CardService from "../services/CardService.js";
 
 export default {
-  
   name: "home",
   data() {
     return {
@@ -50,5 +54,29 @@ export default {
 </script>
 
 <style>
+.home {
+  column-gap: 25px;
+  row-gap: 25px;
+  align-items: center;
+}
 
+.deck-display {
+  display: flex;
+  flex-wrap: wrap;
+  column-gap: 10px;
+  row-gap: 10px;
+  justify-content: center;
+  align-items: center;
+}
+
+.deck {
+  border-radius: 15px;
+  padding: 10px;
+  margin: 10px;
+  border: 10px;
+  width: 150px;
+  background-color: rgb(129, 163, 255);
+  text-align: center;
+  font-family: Arial, Helvetica, sans-serif;
+}
 </style>
